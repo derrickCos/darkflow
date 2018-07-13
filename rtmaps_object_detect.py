1e4dc873357ba00da4f15999626f78f169479b7f
derrickCos <cosmas.1@osu.edu>
2018-07-13 14:52:06 -0400

    Add files via upload

--- rtmaps_object_detect.py
+++ rtmaps_object_detect.py
@@ -8,80 +8,44 @@ import numpy as np
 class rtmaps_python(BaseComponent):
     def __init__(self):
         BaseComponent.__init__(self) # call base class constructor
-        self.add_input("img", rtmaps.types.ANY)  # original image without resiz    
-        self.add_output("coord", rtmaps.types.INTEGER32, 15)  # define detection_result
+        self.add_input("img", rtmaps.types.ANY)  # original image without resizing
+        # Buffer size of 100 = maximum of 25 bounding boxes
+        self.add_output("coord", rtmaps.types.INTEGER32, 100)  # define detection_result
         # Properties name cannot contain spaces
         self.add_property("threshold", 0.5)  # define threshold, max 1.0
         self.add_property("gpu_usage", 0.8)
-        self.add_property("image_height", 480)
-        self.add_property("image_width", 640)
 
     # Birth() will be called once at diagram execution startup
     def Birth(self):
 
         print('Python Birth')
-        options = {"pbLoad": "C:/Program Files/Intempora/RTMaps 4/packages/rtmaps_object_detect/built_graph/yolo.pb",
-                   "metaLoad": "C:/Program Files/Intempora/RTMaps 4/packages/rtmaps_object_detect/built_graph/yolo.meta",
-                   "threshold": self.properties["threshold"].data,
-                   "gpu": self.properties["gpu_usage"].data}
+        options = {"pbLoad": "C:/Program Files/Intempora/RTMaps 4/packages/rtmaps_object_detect/darkflow-master/built_graph/yolo.pb",
+                    "metaLoad": "C:/Program Files/Intempora/RTMaps 4/packages/rtmaps_object_detect/darkflow-master/built_graph/yolo.meta",
+                    "threshold": self.properties["threshold"].data,
+                    "gpu": self.properties["gpu_usage"].data}
 
         self.tfnet = TFNet(options)
 
+
     # Core() is called every time you have a new input
     def Core(self):
-        resized = self.inputs["img"].ioelt.data  # create an ioelt from resized image at 608 x 608 pixels
-        h_original = self.properties["image_height"].data  # height and width from original image before resize
-        w_original = self.properties["image_width"].data
-        print(resized.image_data.shape)
-
+        image = self.inputs["img"].ioelt.data  # create an ioelt from resized image at 608 x 608 pixels
         boxes = rtmaps.types.Ioelt()
         boxes.ts = self.inputs["img"].ioelt.ts
         boxes.data = []
 
-        detection_result, json = self.tfnet.return_predict(resized.image_data, h_original, w_original)
+        # Input image will be the resized image at 608,608 while the input dimensions will be that of the original.
+        # Format of 'detection_result':
+        # label, confidence, topleft_x / x1, topleft_y /  y1, botrightx / x2, botright_y / y2
+        detection_result, json = self.tfnet.return_predict(image.image_data)
 
-        try:
-            boxes.data = []
-            if detection_result[0] is not None and detection_result[0][1] >= self.properties["threshold"].data:
-                boxes.data.append(detection_result[0][2])
-                boxes.data.append(detection_result[0][3])
-                boxes.data.append(detection_result[0][4])
-                boxes.data.append(detection_result[0][5])
-
-        except IndexError:
-            boxes.data.insert(0, 0)
-            boxes.data.insert(1, 0)
-            boxes.data.insert(2, 0)
-            boxes.data.insert(3, 0)
-
-        try:
-            if detection_result[1] is not None and detection_result[1][1] >= self.properties["threshold"].data:
-                boxes.data.append(detection_result[1][2])
-                boxes.data.append(detection_result[1][3])
-                boxes.data.append(detection_result[1][4])
-                boxes.data.append(detection_result[1][5])
-
-        except IndexError:
-            boxes.data.insert(4, 0)
-            boxes.data.insert(5, 0)
-            boxes.data.insert(6, 0)
-            boxes.data.insert(7, 0)
-
-        try:
-            if detection_result[2] is not None and detection_result[2][1] >= self.properties["threshold"].data:
-                boxes.data.append(detection_result[2][2])
-                boxes.data.append(detection_result[2][3])
-                boxes.data.append(detection_result[2][4])
-                boxes.data.append(detection_result[2][5])
-
-        except IndexError:
-            boxes.data.insert(8, 0)
-            boxes.data.insert(9, 0)
-            boxes.data.insert(10, 0)
-            boxes.data.insert(11, 0)
+        for row, var in enumerate(detection_result):
+            boxes.data.append(detection_result[row][2])
+            boxes.data.append(detection_result[row][3])
+            boxes.data.append(detection_result[row][4])
+            boxes.data.append(detection_result[row][5])
 
         print(boxes.data)
-
         self.outputs["coord"].write(boxes)
 
 # Death() will be called once at diagram execution shutdown
